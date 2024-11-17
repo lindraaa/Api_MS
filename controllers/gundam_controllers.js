92321e2f0739e2ebcb680b5196d19cee6b2da3d8
unknown <chancruz739@gmail.com>
2024-11-17 10:26:37 +0800

    added models with example database

--- controllers/gundam_controllers.js
+++ controllers/gundam_controllers.js
@@ -1,4 +1,13 @@
-module.exports.index = (req,res)=>{
-    res.json("FROM ROUTER")
+const Gundam = require("../models/gundam_models")
+
+module.exports.index = async (req,res)=>{
+    const MobileSuit = new Gundam({
+        name:"Strike Freedom Gundam",
+        description:"From Gundam Seed Destiny Series",
+        catergory:"HG",
+        price:1000
+    })
+    await MobileSuit.save()
+    res.json(MobileSuit)
 
 }
