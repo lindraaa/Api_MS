dac8e7703d2c48bcc57f76327e376916290f3539
unknown <chancruz739@gmail.com>
2024-11-17 09:47:47 +0800

    added routes and controllers separately

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express()
 const PORT  = 9000;
+const gundam_routes = require("./routes/gundam_routes")
 
+app.use("/",gundam_routes)
 
 
 app.listen(PORT,()=>{
