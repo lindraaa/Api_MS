211db54709e4328260e188f81a9f649e28bf298b
unknown <chancruz739@gmail.com>
2024-11-17 10:06:47 +0800

    connected to database

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express()
 const PORT  = 9000;
+const mongoose = require("mongoose");
+
+//routes
 const gundam_routes = require("./routes/gundam_routes")
+
+mongoose.connect("mongodb://localhost:27017/MSuit_Api")
+    .then(()=>{
+        console.log("Database Connected")
+    })
+    .catch(err=>{
+        console.log("Connection Failed", err)
+    })
+
+
 
 app.use("/",gundam_routes)
 
